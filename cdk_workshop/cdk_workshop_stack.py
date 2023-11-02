f918a3722b0dea8ef4bb5890f1c94f196183fe6d
Martin Ansong <martin.ansong@harness.io>
2023-11-02 08:11:00 +0000

    Clean up

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,26 +1,12 @@
+from aws_cdk import Duration, Stack
+from aws_cdk import aws_iam as iam
+from aws_cdk import aws_sns as sns
+from aws_cdk import aws_sns_subscriptions as subs
+from aws_cdk import aws_sqs as sqs
 from constructs import Construct
-from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-)
 
 
 class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
